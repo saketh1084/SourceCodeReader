b44772515eb6ab5f01c8a66b76233c6f3c3f6ca7
kamatam sai saketh goud <kamatamsaketh@gmail.com>
2023-12-30 12:18:44 +0530

    First commit

---
+++ src/techi/Name.java
@@ -0,0 +1,9 @@
+package techi;
+public class Name {
+public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("Sai Saketh");
+
+	}
+
+}
---
+++ src/techi/Pact.java
@@ -0,0 +1,76 @@
+package techi;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Pact {
+
+    public static int getTotalLines(String filePath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            int totalLines = 0;
+            while (reader.readLine() != null) {
+                totalLines++;
+            }
+            return totalLines;
+        }
+    }
+
+    public static int getLinesOfCode(String filePath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            int linesOfCode = 0;
+            String line;
+            boolean inCommentBlock = false;
+
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+
+                
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                // Exclude single-line comments
+                if (line.startsWith("//")) {
+                    continue;
+                }
+
+                // Exclude start of multi-line comment
+                if (line.startsWith("/*")) {
+                    inCommentBlock = true;
+                    continue;
+                }
+
+                // Exclude end of multi-line comment
+                if (line.endsWith("*/")) {
+                    inCommentBlock = false;
+                    continue;
+                }
+
+                // Exclude lines within multi-line comment
+                if (inCommentBlock) {
+                    continue;
+                }
+
+                linesOfCode++;
+            }
+            return linesOfCode;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            String filePath = "src/techi/Name.java";
+
+            int totalLines = getTotalLines(filePath);
+            int linesOfCode = getLinesOfCode(filePath);
+
+            System.out.println("Total Lines: " + totalLines);
+            System.out.println("Lines of Code: " + linesOfCode);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
